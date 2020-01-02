67a2e9657e2cbff510710ef3951b1ec6c2c6e8d6
ddori <6290071+hiphopddori@users.noreply.github.com>
2020-01-02 12:55:07 +0900

    Update alertPlugin.js
    refactoring

--- alertPlugin.js
+++ alertPlugin.js
@@ -1,6 +1,6 @@
 /**
  * Created by kic(ddori) on 2019. 12. 17..
- * Admin2 custom alrt , confirm plugin
+ * Admin2 custom alrt , confirm, loading plugin
  */
 
 const alertPlugin = {
@@ -18,26 +18,26 @@ const alertPlugin = {
             //const mainApp = document.getElementById("app");
             //mainApp.appendChild(div);
             document.body.appendChild(div);
-            return div
+            return div;
         }
 
         /* 동적 component Inject
         */
         const componentInject = (component) =>{
             const container = createDivInBody();
-            Vue.prototype.$alertContainer = new Vue({
+            Vue.prototype[component] = new Vue({
                 parent: Plugin.rootInstance,
                 render: h => h(component)
-            }).$mount(container)
+            }).$mount(container);
         }
 
         /* alert component 객체를 얻는다.
         */
-        const getAlertComponent = (root)=> {
-            if (Vue.prototype.$alertContainer == null){
-                componentInject('common-alert');
+        const getInjectComponent = (root, componentName)=> {
+            if (!Vue.prototype.hasOwnProperty(componentName)){
+                componentInject(componentName);
             }
-            return root._dynamicContainer;
+            return root[componentName];
         }
 
         /* 공통 alert 메소드
@@ -45,20 +45,34 @@ const alertPlugin = {
         Vue.prototype.$alert = (msg,width,height) =>{
 
             const root = Plugin.rootInstance;
-            const alertComponent = getAlertComponent(root);
-            if (!alertComponent) return;
+            const alertComponent = getInjectComponent(root,'commonAlert');
+            if (!alertComponent) return
 
-            return alertComponent.alert(false,msg,width,height);
+            return alertComponent.alert(false,"알 림",msg,width,height);
 
         }
 
-        Vue.prototype.$confirm = (msg,width,height) =>{
+        Vue.prototype.$confirm = (msg,title='알림',width,height) =>{
 
             const root = Plugin.rootInstance;
-            const alertComponent = getAlertComponent(root);
+            const alertComponent = getInjectComponent(root,'commonAlert');
             if (!alertComponent) return;
+            /*
+            if (title === undefined){
+                title = "알 림"
+            }
+            */
 
-            return alertComponent.alert(false,msg,width,height);
+            return alertComponent.alert(true,title,msg,width,height);
+
+        }
+
+        Vue.prototype.$loading = (isLoading) =>{
+            const root = Plugin.rootInstance;
+            const loadingComponent = getInjectComponent(root, 'loading');
+            if (!loadingComponent) return;
+
+            return loadingComponent.on(isLoading);
 
         }
 
@@ -70,7 +84,8 @@ const alertPlugin = {
             }
         })
 
-        Vue.prototype.$alertContainer = null;
+        //Vue.prototype["commonAlert"] = null;
+        //Vue.prototype["loading"] = null;
     }
 
 }
